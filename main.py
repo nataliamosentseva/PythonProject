fb064552ae961f44d1270a7ccc1855299b53160a
natalia mosentseva <natashkask09@gmail.com>
2025-02-16 18:47:05 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number, mask_account_card
 
 print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
+print(mask_account_card())
