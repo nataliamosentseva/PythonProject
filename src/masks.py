32ea3011e710c36800b2b66e97d40b69abcfbb5e
natalia mosentseva <natashkask09@gmail.com>
2025-02-16 23:32:14 +0300

    add src/masks/py

--- src/masks.py
+++ src/masks.py
@@ -2,6 +2,7 @@ cadr_number = "7000792289606361"
 
 
 def get_mask_card_number(card_number: str) -> str | None:
+
     """Функция принимает на вход номер карты и возвращает ее маску.
     Номер карты замаскирован и отображается в формате XXXX XX** **** XXXX"""
     if card_number.isdigit() and len(card_number) == 16:
@@ -10,15 +11,14 @@ def get_mask_card_number(card_number: str) -> str | None:
         return None
 
 
-
-
 account_number = "73654108430135874305"
 
-def get_mask_account(account_number: str) -> str | None:
-    """ Функция принимает на вход номер счета и возвращает его маску. Номер счета замаскирован и
-        отображается в формате **XXXX"""
 
+def get_mask_account(account_number: str) -> str | None:
+
+    """ Функция принимает на вход номер счета и возвращает его маску.
+    Номер счета замаскирован и отображается в формате **XXXX"""
     if account_number.isdigit() and len(account_number) == 20:
         return f"{'*' * 2} {account_number[-4::]}"
     else:
-         return None
+        return None
