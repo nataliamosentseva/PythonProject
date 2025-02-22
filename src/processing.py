ed0ebfec0cabd6d9c3adcddc3c90da439b261770
natalia mosentseva <natashkask09@gmail.com>
2025-02-23 00:59:09 +0300

    измененный processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,14 @@
 from typing import List, Dict, Any
 
 
-def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
-    """
-    Фильтрует список словарей по значению ключа 'state'.
-
-    :param data: Список словарей для фильтрации.
-    :param state: Значение ключа 'state' для фильтрации (по умолчанию 'EXECUTED').
-    :return: Новый список словарей, соответствующих указанному значению 'state'.
-    """
-    return [item for item in data if item.get('state') == state]
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Функция, которая принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению."""
+    return [item for item in data if item.get("state") == state]
 
 
-input_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
+def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
+    """Функция, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате"""
+    return sorted(data, key=lambda x: x["date"], reverse=descending)
