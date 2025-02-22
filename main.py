22b0535073be161cbf36b716dcc3c4c0697ad965
natalia mosentseva <natashkask09@gmail.com>
2025-02-23 02:10:29 +0300

    add main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
 print(filtered_data)
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 8990922113665229"))
     print(mask_account_card("Maestro 1596837868705199"))
     print(mask_account_card("Счет 73654108430135874305"))
